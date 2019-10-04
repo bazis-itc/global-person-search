e23d7462fedffe90b9208971afdaeab82d76a95f
Vladislav Ivanov <ivawlal@gmail.com>
2019-10-04 15:55:15 +0300

    add uln module

---
+++ uln/src/main/java/bazis/utils/GlobalPersonSearchUtil.java
@@ -0,0 +1,11 @@
+package bazis.utils;
+
+import bazis.utils.global_person_search.BaseSearchUtil;
+
+public final class GlobalPersonSearchUtil extends BaseSearchUtil {
+
+    public GlobalPersonSearchUtil() {
+        super("http://172.17.60.6/central/");
+    }
+
+}
