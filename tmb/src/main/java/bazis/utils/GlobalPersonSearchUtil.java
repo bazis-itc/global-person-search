e72c19d3576f4939732ca9a3b2715845d95a3b90
Vladislav Ivanov <ivawlal@gmail.com>
2019-10-09 11:51:49 +0300

    switch to production server on tambov

--- tmb/src/main/java/bazis/utils/GlobalPersonSearchUtil.java
+++ tmb/src/main/java/bazis/utils/GlobalPersonSearchUtil.java
@@ -5,7 +5,7 @@ import bazis.utils.global_person_search.BaseSearchUtil;
 public final class GlobalPersonSearchUtil extends BaseSearchUtil {
 
     public GlobalPersonSearchUtil() {
-        super("http://10.65.12.11:8080/update_test_central/");
+        super("http://10.65.12.11:8080/central");
     }
 
 }
