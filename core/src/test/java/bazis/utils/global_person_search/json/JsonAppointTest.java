4886dcf29f3200f36a4a40935ddf547e0d985b8e
Vladislav Ivanov <ivawlal@gmail.com>
2019-11-29 13:09:17 +0300

    add tests for JsonAppoint

--- core/src/test/java/bazis/utils/global_person_search/json/JsonAppointTest.java
+++ core/src/test/java/bazis/utils/global_person_search/json/JsonAppointTest.java
@@ -1,14 +1,67 @@
 package bazis.utils.global_person_search.json;
 
+import bazis.cactoos3.exception.BazisException;
 import bazis.cactoos3.text.FormattedText;
+import bazis.utils.global_person_search.Appoint;
 import bazis.utils.global_person_search.ext.ConcatedText;
 import bazis.utils.global_person_search.ext.Lines;
 import bazis.utils.global_person_search.fake.FakeAppoint;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public final class JsonAppointTest {
+
+    @SuppressWarnings("SpellCheckingInspection")
+    private static final DateFormat DATE_FORMAT =
+        new SimpleDateFormat("yyyy-MM-dd");
+
+    @Test
+    public void canPrintAndParse() throws BazisException {
+        final Appoint
+            origin = new FakeAppoint(),
+            converted = new JsonAppoint(
+                new JsonText(
+                    new JsonText(new JsonAppoint(origin)).asString()
+                ).asJson()
+            );
+        MatcherAssert.assertThat(
+            "test type",
+            converted.type(), Matchers.equalTo(origin.type())
+        );
+        MatcherAssert.assertThat(
+            "test msp",
+            converted.msp(), Matchers.equalTo(origin.msp())
+        );
+        MatcherAssert.assertThat(
+            "test category",
+            converted.category(), Matchers.equalTo(origin.category())
+        );
+        MatcherAssert.assertThat(
+            "test child",
+            converted.child(), Matchers.equalTo(origin.child())
+        );
+        MatcherAssert.assertThat(
+            "test status",
+            converted.status(), Matchers.equalTo(origin.status())
+        );
+        MatcherAssert.assertThat(
+            "test startDate",
+            JsonAppointTest.DATE_FORMAT.format(converted.startDate().get()),
+            Matchers.equalTo(
+                JsonAppointTest.DATE_FORMAT.format(origin.startDate().get())
+            )
+        );
+        MatcherAssert.assertThat(
+            "test endDate",
+            JsonAppointTest.DATE_FORMAT.format(converted.endDate().get()),
+            Matchers.equalTo(
+                JsonAppointTest.DATE_FORMAT.format(origin.endDate().get())
+            )
+        );
+    }
 
     @Test
     public void payments() throws Exception {
@@ -46,6 +99,25 @@ public final class JsonAppointTest {
                     "\""
                 ).asString()
             )
+        );
+    }
+
+    @Test
+    public void emptyDates() throws BazisException {
+        final Appoint
+            origin = new FakeAppoint("some msp", "", ""),
+            converted = new JsonAppoint(
+                new JsonText(
+                    new JsonText(new JsonAppoint(origin)).asString()
+                ).asJson()
+            );
+        MatcherAssert.assertThat(
+            "start date not empty",
+            converted.startDate().has(), Matchers.is(false)
+        );
+        MatcherAssert.assertThat(
+            "end date not empty",
+            converted.endDate().has(), Matchers.is(false)
         );
     }
 
